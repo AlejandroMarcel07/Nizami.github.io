0578f8e50edd2d8760da1b8336b44ac6a2d5979a
marcel zuniga <alejocrak321zuniga@gmail.com>
2023-12-23 11:34:54 -0600

    mejorando el modo oscuro

--- script.js
+++ script.js
@@ -24,27 +24,39 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
-  // Obtener el interruptor de modo oscuro
+// Obtener el interruptor de modo oscuro
 const modoOscuroSwitch = document.getElementById('modoOscuroSwitch');
-
 // Obtener la barra de navegación
 const navbar = document.querySelector('.navbar');
 
+// Verificar si el modo oscuro está activo en el almacenamiento local al cargar la página
+window.addEventListener('DOMContentLoaded', () => {
+  const modoOscuroActivo = localStorage.getItem('modoOscuro') === 'true';
 
-
-// Escuchar cambios en el interruptor
-modoOscuroSwitch.addEventListener('change', () => {
-  document.body.classList.toggle('dark-mode'); // Alternar la clase dark-mode en el body
+  // Aplicar el modo oscuro si está activo en el almacenamiento local
+  if (modoOscuroActivo) {
+    document.body.classList.add('dark-mode');
+    navbar.classList.remove('navbar-light', 'bg-light');
+    navbar.classList.add('navbar-dark', 'bg-dark');
+    modoOscuroSwitch.checked = true;
+  }
 });
 
-
 // Escuchar cambios en el interruptor
 modoOscuroSwitch.addEventListener('change', () => {
+  // Alternar la clase dark-mode en el body
+  document.body.classList.toggle('dark-mode');
+
+  // Modificar estilos de la barra de navegación
   if (modoOscuroSwitch.checked) {
     navbar.classList.remove('navbar-light', 'bg-light');
     navbar.classList.add('navbar-dark', 'bg-dark');
+    localStorage.setItem('modoOscuro', 'true'); // Guardar estado en el almacenamiento local
   } else {
     navbar.classList.remove('navbar-dark', 'bg-dark');
     navbar.classList.add('navbar-light', 'bg-light');
+    localStorage.setItem('modoOscuro', 'false'); // Guardar estado en el almacenamiento local
   }
 });
+
+
